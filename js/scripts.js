889bd1f44ec32d01002b27b19b9844dd55e8b52b
kaczor6418 <andrzej6418@gmail.com>
2018-05-07 18:11:22 +0200

    st is wrong with _open

--- js/scripts.js
+++ js/scripts.js
@@ -28,6 +28,10 @@ AJAX({
 
    this._xhr = new XMLHttpRequest();
    this.config = this._extendOptions(config);
+   this._assingEvents();
+   this._open();
+   this._assingUserHeaders();
+   this._send();
 
    console.log(this._defaultConfig);
  }
@@ -56,9 +60,48 @@ AJAX({
    return defaultConfig;
  };
 
+ AJAX.prototype._open = function () {
+   this._xhr.open(
+     this._config.type,
+     this._config.url,
+     this._config.options.async,
+     this._config.options.username,
+     this._config.type.options.password
+   );
+   this._xhr.timeout = this._config.options.timeout;
+ };
+
+ AJAX.prototype._assingEvents = function () {
+   this._xhr.addEventListener("readystatechange", this._handleResponse.bind(this), false);
+   this._xhr.addEventListener("error", this._handleError.bind(this), false);
+   this._xhr.addEventListener("abort", this._handleError.bind(this), false);
+   this._xhr.addEventListener("timeout", this._handleError.bind(this), false);
+ };
+
+ AJAX.prototype._send = function () {
+   this._xhr.send();
+ };
+
+ AJAX.prototype._assingUserHeaders = function (e) {
+   if(Object.keys(this._config.headers).length){
+     for (let key in this._config.headers) {
+       this._xhr.setRequestHeader(key, this._config.headers[key]);
+     }
+   }
+ };
+
+ AJAX.prototype._handleResponse = function (e) {
+   if (this._xhr.readyState === 4 && this._xhr.status === 200) {
+     console.log("Elo");
+   }
+ };
+
+ AJAX.prototype._handleError = function (e) {
+
+ };
 
 
- var a = AJAX({
+AJAX({
      type: "POST",
      url: "odbierz.php",
      data: {
